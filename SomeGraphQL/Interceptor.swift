83a4073fe987376971d4ce655868fbec91259799
Oleksandr Bezpalchuk <oleksandr.bezpalchuk@ekreative.com>
2021-03-27 15:28:35 +0200

    Use github api with graphql to search, star and unstar repositories

---
+++ SomeGraphQL/Interceptor.swift
@@ -0,0 +1,51 @@
+//
+//  Interceptor.swift
+//  SomeGraphQL
+//
+//  Created by Oleksandr on 27.03.2021.
+//
+
+import Apollo
+
+struct NetworkInterceptorProvider: InterceptorProvider {
+
+    // These properties will remain the same throughout the life of the `InterceptorProvider`, even though they
+    // will be handed to different interceptors.
+    private let store: ApolloStore
+    private let client: URLSessionClient
+
+    init(store: ApolloStore,
+         client: URLSessionClient) {
+        self.store = store
+        self.client = client
+    }
+
+    func interceptors<Operation: GraphQLOperation>(for operation: Operation) -> [ApolloInterceptor] {
+        return [
+            MaxRetryInterceptor(),
+            LegacyCacheReadInterceptor(store: self.store),
+            UserManagementInterceptor(),
+            NetworkFetchInterceptor(client: self.client),
+            ResponseCodeInterceptor(),
+            LegacyParsingInterceptor(cacheKeyForObject: self.store.cacheKeyForObject),
+            AutomaticPersistedQueryInterceptor(),
+            LegacyCacheWriteInterceptor(store: self.store)
+        ]
+    }
+}
+
+
+class UserManagementInterceptor: ApolloInterceptor {
+
+    func interceptAsync<Operation: GraphQLOperation>(
+        chain: RequestChain,
+        request: HTTPRequest<Operation>,
+        response: HTTPResponse<Operation>?,
+        completion: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) {
+        request.addHeader(name: "Authorization", value: "Bearer \(Const.githubToken)")
+        chain.proceedAsync(request: request,
+                           response: response,
+                           completion: completion)
+
+    }
+}
